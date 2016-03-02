98f9bbc55a15169b2599776f73eaa09d78937cc2
Vinny <vin_palumbo@hotmail.com>
2016-03-02 14:03:50 -0500

    Remove Back button in Book Detail view

--- app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -119,11 +119,6 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
 
         String categories = data.getString(data.getColumnIndex(AlexandriaContract.CategoryEntry.CATEGORY));
         ((TextView) rootView.findViewById(R.id.categories)).setText(categories);
-
-        if(rootView.findViewById(R.id.right_container)!=null){
-            rootView.findViewById(R.id.backButton).setVisibility(View.INVISIBLE);
-        }
-
     }
 
     @Override
